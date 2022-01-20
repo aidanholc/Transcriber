43a383df750ea1fc03a20cd2679e3156a0f52fc6
Aidan <aidanholc@gmail.com>
2022-01-19 21:11:38 -0600

    Created Note.java & WholeNote.java

--- Note.java
+++ Note.java
@@ -1,1 +1,6 @@
+/**
+ * Something smart here about how this is the abstract class
+ */
 
+public abstract class Note {
+}
---
+++ WholeNote.java
@@ -0,0 +1,7 @@
+/**
+ * Something smart here about what this is going to do
+ */
+
+public class WholeNote extends Note {
+
+}
